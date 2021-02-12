3709571111bda4cc18a2c767dbf951d74b57843a
armando <borelliarmando12@gmail.com>
2021-02-12 16:48:44 -0600

    Added success.html to redirect after form is submitted, to prevent data duplicates

--- dojo_survey_app/views.py
+++ dojo_survey_app/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render, HttpResponse, redirect
 
 
 def index(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
 
 def create_user(request):
     print("Got Post Info....................")
-    print(request.POST)
-    return render(request, "index.html")
+    name_from_form = request.POST['name']
+    email_from_form = request.POST['email']
+    context = {
+        "name_on_template": name_from_form,
+        "email_on_template": email_from_form
+    }
+
+    return redirect("/success")
+
+def success(request):
+
+    return render(request, "success.html")
+
+
 
