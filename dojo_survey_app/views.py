583e028da14cb2f7c88c5beddbd85b99c5adb751
armando <borelliarmando12@gmail.com>
2021-02-12 19:46:50 -0600

    last commit

--- dojo_survey_app/views.py
+++ dojo_survey_app/views.py
@@ -5,7 +5,25 @@ def index(request):
     return render(request, "index.html")
 
 
-def survey(request):
+# def survey(request):
+#     name_form = request.POST['name']
+#     email_form = request.POST['email']
+#     location_form = request.POST['location']
+#     favorite_lang_form = request.POST['favorite_language']
+#     comments_form = request.POST['comment']
+
+#     context = {
+#         "name": name_form,
+#         "email": email_form,
+#         "location": location_form,
+#         "favorite_lang": favorite_lang_form,
+#         "comments": comments_form
+#     }
+#     print(context)
+
+#     return redirect("/result", context)
+
+def success(request):
     name_form = request.POST['name']
     email_form = request.POST['email']
     location_form = request.POST['location']
@@ -13,21 +31,13 @@ def survey(request):
     comments_form = request.POST['comment']
 
     context = {
-        "name": name_form, 
-        "email": email_form, 
-        "location": location_form, 
-        "favorite_lang": favorite_lang_form, 
+        "name": name_form,
+        "email": email_form,
+        "location": location_form,
+        "favorite_lang": favorite_lang_form,
         "comments": comments_form
     }
+
     print(context)
 
-    return redirect("/result", context)
-
-def success(request):
-
-    # print("Context is in success now: " + context)
-
-    return render(request, "result.html")
-
-
-
+    return render(request, "result.html", context)
